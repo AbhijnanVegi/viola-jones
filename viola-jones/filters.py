fa38b804deea2fe7f5b3f8b04e3f4c2ed3ef93d0
Abhijnan Vegi <abhijnan.vegi@gmail.com>
2022-11-22 20:27:00 +0530

    Modified regional sum computation

--- viola-jones/filters.py
+++ viola-jones/filters.py
@@ -5,28 +5,33 @@ from integral_image import integral_image
 def regional_sum(iimg: np.ndarray, top_left: tuple, bottom_right: tuple) -> float:
     """
     Calculates the sum of a region given an integral image.
-    Doesn't include row and column of top_left.
     @iimg: Integral image representation of image
     @top_left: tuple (x,y) representing top left corner of image
     @bottom_right: tuple (x,y) representing bottom right corner of image 
     """
-    top_left = (top_left[0]-1, top_left[1]-1)
 
     if top_left == bottom_right:
         return iimg[top_left]
 
-    # calculate other endpoints of the region
-    top_right = (bottom_right[0], top_left[1])
-    bottom_left = (top_left[0], bottom_right[1])
+    # calculate the sum of the region
+    sum = iimg[bottom_right]
 
-    return iimg[top_left] + iimg[bottom_right] - (iimg[top_right] + iimg[bottom_left])
+    if top_left[0] > 0:
+        sum -= iimg[top_left[0]-1, bottom_right[1]]
+    if top_left[1] > 0:
+        sum -= iimg[bottom_right[0], top_left[1]-1]
+    if top_left[0] > 0 and top_left[1] > 0:
+        sum += iimg[top_left[0]-1, top_left[1]-1]
+
+    return sum
 
 def test_regional_sum():
 
     iimg = np.array([[1, 3, 6], [5, 12, 21], [12, 27, 45]])
 
-    s = regional_sum(iimg, (0,0), (2,2))
-    print(s)
+    s = regional_sum(iimg, (1,1), (2,2))
+    assert(s == 28)
+    print("Regional sum test passed")
 
 if __name__ == "__main__":
     test_regional_sum()
