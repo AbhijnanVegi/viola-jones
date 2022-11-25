871013f729a206ae8717c972795ab50959c32db8
Abhijnan Vegi <abhijnan.vegi@gmail.com>
2022-11-25 17:44:00 +0530

    Added weak classifier training

--- viola-jones/train.py
+++ viola-jones/train.py
@@ -2,11 +2,21 @@ from integral_image import integral_image
 from filters import *
 import numpy as np
 
+class WeakClassifier():
+    def __init__(self, feature, threshold, polarity) -> None:
+        self.feature = feature
+        self.threshold = threshold
+        self.polarity = polarity
+
+    def predict(self, iimg):
+        score = self.feature.apply(iimg)
+        return 1 if self.polarity * score < self.polarity * self.threshold else 0
+
 class RapidObjectDetector:
     def __init__(self, layers):
         self.layers = layers
 
-    def train(self, X, y) :
+    def train(self, X, y, rounds=10) :
         h = X.shape[0]
         w = X.shape[1]
 
@@ -31,5 +41,57 @@ class RapidObjectDetector:
                 weights[i] = 1 / (2 * negatives)
 
         print(positives, negatives)
+
+    def _train_weak(self, X_ff, y, weights, features):
+        
+        pos, neg = 0,0
+        
+        for i, y in enumerate(y):
+            if y == 1:
+                pos += weights[i]
+            else:
+                neg += weights[i]
+
+        clfs = []
+
+        for i, feature in enumerate(features):
+
+            curr_feature_scores = []
+            for j, x in enumerate(X_ff):
+                curr_feature_scores.append((x[i], y[j], weights[j]))
+
+            min_error = float('inf')
+            best_threshold = 0
+            best_polarity = 0
+            best_feature = None
+
+            posw, negw = 0,0
+            poss, negs = 0,0
+
+            for x, y, w in sorted(curr_feature_scores, key=lambda x: x[0]):
+                error = min(negw + pos - posw, posw + neg - negw)
+                if error < min_error:
+                    min_error = error
+                    best_threshold = x
+                    best_polarity = 1 if poss > negs else -1
+                    best_feature = feature
+
+                if y == 1:
+                    posw += w
+                    poss += 1
+                else:
+                    negw += w
+                    negs += 1
+            
+            clfs.append(WeakClassifier(best_feature, best_threshold, best_polarity))
+
+        return clfs
+
+
+
+
+
+
+
     
         
